245a7e01efb59aa30951958205c14d1c7b52e6a5
tcmulder <tcmulder@gmail.com>
2017-01-16 09:35:04 -0700

    Set up global vars for css

--- wp-content/themes/legion/ap_lib/ap_google_fonts.php
+++ wp-content/themes/legion/ap_lib/ap_google_fonts.php
@@ -1,6 +1,6 @@
 <?php
 function assets() {
-  $googlefontlist = 'Open+Sans:400,700,300';
+  $googlefontlist = 'Droid+Sans:700|Open+Sans:400,700,700i';
   wp_enqueue_style('apl_google_font', '//fonts.googleapis.com/css?family='. $googlefontlist, false, NULL);
 }
 add_action('wp_enqueue_scripts', 'assets', 100);
