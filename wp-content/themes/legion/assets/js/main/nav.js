fd8ddcbbb24670cac3e92e2c9f537dda0fc43489
tcmulder <tcmulder@gmail.com>
2017-01-16 15:53:20 -0700

    Rename nav js

--- wp-content/themes/legion/assets/js/main/nav.js
+++ wp-content/themes/legion/assets/js/main/nav.js
@@ -1,9 +1,8 @@
+/*------------------------------------*\
+    ::Navigation
+\*------------------------------------*/
 jQuery(document).ready(function ($) {
     'use strict';
-
-    /*------------------------------------*\
-        ::Navigation
-    \*------------------------------------*/
     $('#js-nav-trigger').on('click', function(){
         $(this).toggleClass('active');
     });
