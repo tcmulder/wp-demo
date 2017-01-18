f873deb66209daf4db941378f8becc05b8392fff
tcmulder <tcmulder@gmail.com>
2017-01-18 14:51:42 -0700

    Add 404 page

---
+++ wp-content/themes/legion/404.php
@@ -0,0 +1,4 @@
+<article class="four-oh-four main-torso__group">
+  <h1>Uh oh...</h1>
+  <p>The page you requested isn't available. Not a problem: I could build that for you! <a href="http://www.tcmulder.com" target="_blank">Get in touch</a>.</p>
+</article>
