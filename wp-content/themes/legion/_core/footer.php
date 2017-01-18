2b38f6f59fd71c1daf543d489618d95b5d4d3380
tcmulder <tcmulder@gmail.com>
2017-01-17 21:24:08 -0700

    Add footer for customizer

--- wp-content/themes/legion/_core/footer.php
+++ wp-content/themes/legion/_core/footer.php
@@ -1,3 +1,4 @@
-<div class="main-foot__inner">
-    ...
+<div class="main-foot__inner main-foot__inner--narrow">
+    <h4><?php echo get_option('footer_title'); ?></h4>
+    <p><?php echo get_option('footer_text'); ?></p>
 </div>
--- wp-content/themes/legion/lib/customizer.php
+++ wp-content/themes/legion/lib/customizer.php
@@ -4,6 +4,8 @@ namespace Roots\Sage\Customizer;
 
 use Roots\Sage\Assets;
 
+use WP_Customize_Control;
+
 /**
  * Add postMessage support
  */
@@ -19,3 +21,45 @@ function customize_preview_js() {
   wp_enqueue_script('sage/customizer', Assets\asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 }
 add_action('customize_preview_init', __NAMESPACE__ . '\\customize_preview_js');
+
+/**
+ * Footer
+ */
+add_action('customize_register', __NAMESPACE__ . '\\footer_customizer');
+function footer_customizer($wp_customize) {
+
+    $wp_customize->add_section( 'footer_content', array(
+        'title'    => 'Footer Content',
+        'priority' => 500, // pretty much the bottom
+    ) );
+
+    $wp_customize->add_setting( 'footer_title', array(
+        'default'    => get_option( 'footer_title' ),
+        'type'       => 'option',
+        'sanitize_callback' => __NAMESPACE__ . '\\sanitize_text',
+        'capability' => 'manage_options',
+    ) );
+
+    $wp_customize->add_control( 'footer_title', array(
+        'label'      => 'Footer Title',
+        'section'    => 'footer_content',
+    ) );
+
+    $wp_customize->add_setting( 'footer_text', array(
+        'default'    => get_option( 'footer_text' ),
+        'type'       => 'option',
+        'sanitize_callback' => __NAMESPACE__ . '\\sanitize_text',
+        'capability' => 'manage_options',
+    ) );
+
+    $wp_customize->add_control( 'footer_text', array(
+        'label'      => 'Footer Text',
+        'type'       => 'textarea',
+        'section'    => 'footer_content',
+    ) );
+
+}
+
+function sanitize_text($text) {
+    return sanitize_text_field($text);
+}
